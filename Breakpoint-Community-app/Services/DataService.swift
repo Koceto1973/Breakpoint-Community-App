12b820d9dd8392c9f1f1fea3d1782f0abf646822
Koceto1973 <koceto1973@yahoo.ca>
2018-11-15 14:00:38 -0800

    db basic refs in

---
+++ Breakpoint-Community-app/Services/DataService.swift
@@ -0,0 +1,37 @@
+//
+//  DataService.swift
+//  Breakpoint-Community-app
+//
+//  Created by K.K. on 15.11.18.
+//  Copyright © 2018 K.K. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+let DB_BASE = Database.database().reference()
+
+class DataService {
+    static let instance = DataService()
+    
+    private var _REF_BASE = DB_BASE
+    private var _REF_USERS = DB_BASE.child("users")
+    private var _REF_GROUPS = DB_BASE.child("groups")
+    private var _REF_FEED = DB_BASE.child("feed")
+    
+    var REF_BASE: DatabaseReference {
+        return _REF_BASE
+    }
+    
+    var REF_USERS: DatabaseReference {
+        return _REF_USERS
+    }
+    
+    var REF_GROUPS: DatabaseReference {
+        return _REF_GROUPS
+    }
+    
+    var REF_FEED: DatabaseReference {
+        return _REF_FEED        
+    }
+}
