f965fb1c8439c78b61008dfae4e19b6c31082e26
Koceto1973 <koceto1973@yahoo.ca>
2018-11-16 20:31:56 -0800

    feed cell in

---
+++ Breakpoint-Community-app/View/FeedCell.swift
@@ -0,0 +1,23 @@
+//
+//  FeedCell.swift
+//  Breakpoint-Community-app
+//
+//  Created by K.K. on 16.11.18.
+//  Copyright © 2018 K.K. All rights reserved.
+//
+
+import UIKit
+
+class FeedCell: UITableViewCell {
+    
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var emailLbl: UILabel!
+    @IBOutlet weak var contentLbl: UILabel!
+    
+    func configureCell(profileImage: UIImage, email: String, content: String) {
+        self.profileImage.image = profileImage
+        self.emailLbl.text = email
+        self.contentLbl.text = content
+    }
+    
+}
